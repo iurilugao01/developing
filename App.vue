8074a2bca1cdce85db818540e1809728e0245298
iurilugao01 <128332733+iurilugao01@users.noreply.github.com>
2023-12-12 19:53:27 -0300

    contador Vue

---
+++ App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      teste: "funciona",
+    };
+  },
+};
+</script>
+
+<template>
+  <button v-on:click="count + 1">Contador: {{ count }}</button>
+</template>
+
+<style>
+#app {
+  display: grid;
+  place-content: center;
+  min-height: 100vh;
+}
+button {
+  font-size: 2rem;
+}
+</style>
